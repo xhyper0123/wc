2502779e24769f437aee0c73abb5f89b996c50af
xhyper0123 <163719449+xhyper0123@users.noreply.github.com>
2024-07-22 01:21:48 +0530

    Update 10.c

--- 10.c
+++ 10.c
@@ -1,31 +1,32 @@
-k=4;
-M =2^k;
-nFFT= 128;
+k = 4;
+M = 2^k; 
+nFFT = 128; 
 cplen = 8; 
-Txaymbols= randi ([0 M-1], nFFT, 1);
-txgrid= qammod (txsymbols, M, UnitAverage Power=true); 
-txout=ifft (txgrid, nFFT); 
-txout=txout(:);
-txcp =txout(nFFT-cplen+1:nFFT);
-txout =[txcp; txout];
-Fs=20e3 *nFFT/2;
- Ts= 1/Fs:
-Tend =Ts *(length (txout)-1);
-subplot (211) ;
-hold off;
-plot (0: Ts: Tend, real (txout)," * ");
+txsymbols = randi([0 M-1],nFFT,1);
+txgrid = qammod(txsymbols,M,UnitAveragePower=true);
+txout = ifft(txgrid,nFFT);
+txout = txout(:); 
+symbols
+txcp = txout(nFFT-cplen+1:nFFT);
+txout = [txcp; txout];
+Fs = 20e3 * nFFT/2; 
+Ts = 1 / Fs; 
+Tend = Ts * (length(txout)-1);
+subplot(211)
+hold off
+plot(0:Ts:Tend,real(txout),"*")
 title("Real component of transmitter output")
-nFFTUP=3* FFT;
-fftgrid =[txgrid(1:nFFT/2);
-txgrid((nFFT/2+1): nFFT)];
-txout =3 *ifft (fftgrid, nFFTUP);
-txout =txout(:);
-cplenUp =cplen *3; 
-txcp =txout (nFFTUp-cplenUp+1:nFFTUp);
-txout= [txcp; txout]; 
-Ts=1/(3*Fs);
-Tend= Ts *(length (txout)-1);
+nFFTUp=3 * nFFT;
+fftgrid = [txgrid(1:nFFT/2);
+ txgrid((nFFT/2+1):nFFT)];
+txout = 3 *ifft(fftgrid,nFFTUp);
+txout = txout(:);
+cplenUp = cplen * 3;
+txcp = txout(nFFTUp-cplenUp+1:nFFTUp);
+txout = [txcp; txout];
+Ts = 1 / (3*Fs);
+Tend = Ts * (length(txout)-1);
 subplot(211)
 hold on
-plot (0:Ts: Tend, real (txout))
-legend ("Original", "Upsampled", "Location","southeast")
+plot(0:Ts:Tend,real(txout))
+legend ("Original","Upsampled","Location","southeast")
