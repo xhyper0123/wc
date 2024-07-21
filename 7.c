7c59a9910f7643ee6b545b3e4e9f62915d6dacd3
xhyper0123 <163719449+xhyper0123@users.noreply.github.com>
2024-07-22 00:43:07 +0530

    Update 7.c

--- 7.c
+++ 7.c
@@ -1,43 +1,70 @@
 clc;
 clear all;
 close all;
-M=4; 
-N=2^M-1; 
-x=input('Enter the initial states of LFSR: ');
-x1=x(1);
-x2=x(2);
-x3=x(3);
-x4=x(4);
-states=[];
-PN_sequence=[];
-for j=1:N
- states=[states;x1(j) x2(j) x3(j) x4(j)];
- x1(j+1)=xor(x3(j),x4(j));
- x2(j+1)=x1(j);
- x3(j+1)=x2(j);
- x4(j+1)=x3(j);
- PN_sequence=[PN_sequence x4(j)];
+%Parameters setting
+M = 2; % BPSK signal constellation
+no_of_data_points = 64; % have 64 data points
+block_size = 8; % size of each ofdm block
+cp_len = ceil(0.1*block_size); % length of cyclic prefix
+no_of_ifft_points = block_size; % 8 points for the FFT/IFFT
+no_of_fft_points = block_size;
+%Transmitter
+% Generate data points
+data_source = randsrc(1, no_of_data_points, 0:M-1);
+figure(1)
+stem(data_source);
+grid on;xlabel('data points');
+ylabel('transmitted data phase representation')
+title('Transmitted Data "O"')
+% Perform BPSK modulation
+Bpsk_modulated_data = pskmod(data_source, M);
+scatterplot(Bpsk_modulated_data);
+title('bpsk modulated transmitted data')
+% Do IFFT on each block
+num_cols=length(Bpsk_modulated_data)/block_size;
+data_matrix = reshape(Bpsk_modulated_data, block_size, num_cols);
+% Create empty matix to put the IFFT'd data
+cp_start = block_size-cp_len;
+cp_end = block_size;
+% Operate columnwise & do CP
+for i=1:num_cols
+ifft_data_matrix(:,i) = ifft((data_matrix(:,i)),no_of_ifft_points);
+% Compute and append Cyclic Prefix
+for j=1:cp_len
+actual_cp(j,i) = ifft_data_matrix(j+cp_start,i);
 end
-figure(1);
-stem(PN_sequence);
-title('PN sequence');
-xlabel('time------->');
-ylabel('Amplitude-------------->');
-for i=1:N
-    if PN_sequence(i)==1
- PN_sequence(i)=-1;
- elseif PN_sequence(i)==0
- PN_sequence(i)=1;
- end
+% Append the CP to the existing block to create the actual OFDM block
+ifft_data(:,i) = vertcat(actual_cp(:,i),ifft_data_matrix(:,i));
 end
-ac=[];
-for lag=-1:N+1
- p=circshift(PN_sequence,lag);
- acf=sum(PN_sequence.*p)/N;
- ac=[ac acf];
+% Convert to serial stream for transmission
+[rows_ifft_data cols_ifft_data]=size(ifft_data);
+len_ofdm_data = rows_ifft_data*cols_ifft_data;
+% OFDM signal to be transmitted
+ofdm_signal = reshape(ifft_data, 1, len_ofdm_data);
+figure(3)
+plot(real(ofdm_signal));
+xlabel('Time');
+ylabel('Amplitude');
+title('OFDM Signal');
+grid on;
+%Receiver
+%Pass the ofdm signal through the channel
+recvd_signal = ofdm_signal;
+%Convert Data back to "parallel" form to perform FFT
+recvd_signal_matrix = reshape(recvd_signal,rows_ifft_data, cols_ifft_data);
+%Remove CP
+recvd_signal_matrix(1:cp_len,:)=[];
+%Perform FFT
+for i=1:cols_ifft_data
+fft_data_matrix(:,i) = fft(recvd_signal_matrix(:,i),no_of_fft_points);
 end
-figure(2);
-plot(-1:N+1,ac,'r-');
-title('Autocorrreation function');
-xlabel('lag------->');
-ylabel('Amplitude-------------->');
+%Convert to serial stream
+recvd_serial_data = reshape(fft_data_matrix, 1,(block_size*num_cols));
+%Demodulate the data
+Bpsk_demodulated_data = pskdemod(recvd_serial_data,M);
+scatterplot(Bpsk_modulated_data);title('Received Bpsk modulated data')
+figure(5)
+stem(Bpsk_demodulated_data,'rx');
+grid on;xlabel('data points');
+ylabel('received data phase representation');
+title('Received Data "X"')
