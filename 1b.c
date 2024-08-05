8d0b125bcc8fa99483afe6c5ffa165b3f8ff2422
xhyper0123 <163719449+xhyper0123@users.noreply.github.com>
2024-08-05 19:03:38 +0530

    Update 1b.c

--- 1b.c
+++ 1b.c
@@ -1,14 +1,15 @@
 pt=500;
+d=0:100:1000;
 gt=1;
 gr=1;
 w=100;
-d=0:50:500
+do=0:50:500
 x=4
 n=2
 pr=pt*gt*gr*(w^2)./(((4*pi)^2)*(d.^2));
 pl_do=10*(pt./pr);
 pl_d=pl_do+10*n*log10(d./do)+x;
-%figure(1);
+figure(1);
 plot(d,pl_d);
 xlabel('distance');
 ylabel('path loss');
