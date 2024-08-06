722c8b82460bfa8d05cc594653250db7a8e67ff3
xhyper0123 <163719449+xhyper0123@users.noreply.github.com>
2024-08-06 06:33:51 +0530

    Update 5.c

--- 5.c
+++ 5.c
@@ -1,18 +1,20 @@
-samples=input('enter no of samples to be considered');
-signal=samples*8;
-PCM=randi([0,1],24,signal);
+clc;
+close all;
+Bits_persignal=8;
+samples=8;
+PCM_signal=randi([0,1], 24,Bits_persignal*samples);
 figure(1);
 for i=1:24
     subplot(24,1,i);
-    stem(pcm(i,:));
-    title('pcm signal');
+    stem(PCM_signal(i,:));
+    title('PCM signal');
 end
 xlabel('bits');
 ylabel('amplitude');
 title('PCM signal');
 figure(2);
-T1_signal=reshape(PCM,1,24*length(PCM));
+T1_signal=reshape(PCM_signal, 1,24*length(PCM_signal));
 stem(T1_signal);
-title('T1 bit stream');
 xlabel('bits');
-ylabel('amplitude')
+ylabel('amplitude');
+title('t1 bit stream');
